690d8f1a44051d510ac5b04cdce32a779bd43529
Brahman-40k <216930549+Brahman-40k@users.noreply.github.com>
2025-10-06 16:25:40 +0200

    Add initial implementation for two numbers calculation

---
+++ Lab02/two_numbers.py
@@ -0,0 +1,19 @@
+# two numbers
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+add = a+b
+diff= a-b
+prod = a*b
+avg = (a+b)/2
+dist = abs(a-b)
+maxnum = max(a,b)
+minnum = min(a,b)
+
+print("Sum:",add)
+print("Difference:",diff)
+print("Average Value:",avg)
+print("Distance:",dist)
+print("Maximum Value:",maxnum)
+print("Minimum Value:",minnum)
