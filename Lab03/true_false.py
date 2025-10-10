bcfa4a0353979e5c55e9706a8cc3787ba95aff1c
Brahman-40k <216930549+Brahman-40k@users.noreply.github.com>
2025-10-10 18:24:56 +0200

    Add initial implementation of true/false comparison in Python

---
+++ Lab03/true_false.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+a = 1
+b = 1
+# a = 1
+# b = 1.0
+#   a = 2.0
+#   b = sqrt(4)
+# a = "1"
+# b = 1
+#   a = "ciao"
+#   b = "Ciao"
+
+if a == b:
+    print(True)
+else:
+    print(False)
