6c62e737189dc96fa36a5882b9560d8201891643
Brahman-40k <216930549+Brahman-40k@users.noreply.github.com>
2025-10-19 13:26:52 +0200

    added an implementation of creating a connection of two lists.

--- Lab09/bar_chart.py
+++ Lab09/bar_chart.py
@@ -1,8 +1,22 @@
 import random as rd
 
+countries = [
+    "United States",
+    "Canada",
+    "Brazil",
+    "United Kingdom",
+    "France",
+    "Germany",
+    "India",
+    "China",
+    "Australia",
+    "South Africa",
+]
 values = []
 for i in range(10):
     values.append(rd.randint(1, 10))
 largest = max(values)
-for i in values:
-    print("*" * int(40 * (i / largest)), f"{int(40 * (i / largest))}")
+
+for i, j in zip(countries, values):
+    bar = "*" * int(40 * (j / largest))
+    print(f"{i}: {bar}")
