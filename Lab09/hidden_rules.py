66342f334a43c216e035a3f5c77951a944c68cb7
Brahman-40k <216930549+Brahman-40k@users.noreply.github.com>
2025-10-19 13:01:56 +0200

    added a program generating sequences with certain rules.

---
+++ Lab09/hidden_rules.py
@@ -0,0 +1,36 @@
+l = []
+
+for i in range(10):
+    l.append(i + 1)
+print(l)
+l = []
+for i in range(11):
+    l.append(2 * i)
+print(l)
+l = []
+for i in range(10):
+    l.append((i + 1) ** 2)
+print(l)
+print(l)
+l = []
+for i in range(10):
+    l.append(0)
+print(l)
+l = []
+l = [1, 4, 9, 16, 9, 7, 4, 9, 11]
+print(l)
+l = []
+for i in range(10):
+    if i % 2 == 0:
+        l.append(0)
+    else:
+        l.append(1)
+print(l)
+l = []
+for i in range(10):
+    if i < 5:
+        l.append(i)
+    else:
+        l.append(i - 5)
+print(l)
+l = []
