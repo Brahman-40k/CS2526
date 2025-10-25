1bbc91b701e06b0c7812e7bdce8f10e6e94cae30
Brahman-40k <216930549+Brahman-40k@users.noreply.github.com>
2025-10-25 13:55:30 +0200

    add beautification statements and logical layout.

--- Lab11/two_strings/app.py
+++ Lab11/two_strings/app.py
@@ -6,11 +6,14 @@ def main():
     s1 = set(s1)
     s2 = set(s2)
     intersection = s1 & s2
+    print("The common characters are: ", end="")
     print(intersection)
     symmetric_difference = s1 ^ s2
+    print("Character that are present in one string not in another: ", end="")
     print(symmetric_difference)
-    all_letters = set(st.ascii_letters.lower())
+    all_letters = set(st.ascii_letters)
     # union
+    print("All the remaining ascii characters: ", end="")
     print(all_letters - s1 | s2)  # '|' is the union operator
 
 
